ab1f181e58aa19b808002e530989668283e5fa3c
athulAnilkumarAppu <athulanilkumarappu@gmail.com>
2024-03-19 22:27:44 +0530

    by id api created

--- src/controllers/fetchLotteryResults.controller.js
+++ src/controllers/fetchLotteryResults.controller.js
@@ -18,5 +18,24 @@ const fetchLotteryResultsApi = async (req, res)=> {
       }
 }
 
+const fetchSingleLotteryByIdApi = async (req, res)=> {
+  const id = req?.body.id
+  try {
+  
+      const snapshot = await admin.database().ref('/lotteryResultsData').once('value');
+  const data = snapshot.val();
 
-module.exports = {fetchLotteryResultsApi: fetchLotteryResultsApi}
+  const lotteryResult = data.lotteries
+
+  const filteredResultById = lotteryResult?.filter((item)=> item?.id === id)
+
+      res.json({status: 200, resultById: filteredResultById})
+     
+    } catch (error) {
+      res.json({status: 500, message: 'Something went wrong, please retry', error: error})
+      throw error;
+    }
+}
+
+
+module.exports = {fetchLotteryResultsApi: fetchLotteryResultsApi, fetchSingleLotteryByIdApi: fetchSingleLotteryByIdApi}
--- src/routes/getLotteryResultsRouter.js
+++ src/routes/getLotteryResultsRouter.js
@@ -6,10 +6,11 @@ const express = require('express')
 
 const getLotteryResultsRouter = express.Router()
 
-const {fetchLotteryResultsApi} = require('../controllers/fetchLotteryResults.controller')
+const {fetchLotteryResultsApi, fetchSingleLotteryByIdApi} = require('../controllers/fetchLotteryResults.controller')
 
 
 
 getLotteryResultsRouter.post('/getLotteryResults', fetchLotteryResultsApi)
+getLotteryResultsRouter.post('/getLotteryResultsById', fetchSingleLotteryByIdApi)
 
 module.exports = getLotteryResultsRouter
